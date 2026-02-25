7d0b099a2e0d280393eeeff6d604ad9362ab3864
Alessio Disarò <40635338+SwatBros@users.noreply.github.com>
2026-02-25 17:15:23 +0100

    feat: add csv connector headers

--- connectors/csv.go
+++ connectors/csv.go
@@ -4,14 +4,49 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type CsvConnector struct {
 	File string
 }
 
+func newCsvConnector(filename string, append bool, headers *[]string) (*CsvConnector, error) {
+	mode := os.O_CREATE | os.O_WRONLY
+	if append {
+		mode |= os.O_APPEND
+	} else {
+		mode |= os.O_TRUNC
+	}
+
+	file, err := os.OpenFile(filename, mode, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file: %w", err)
+	}
+	defer file.Close()
+
+	if headers != nil {
+		headerLine := fmt.Sprintln(strings.Join(*headers, ","))
+		if _, err := file.Write([]byte(headerLine)); err != nil {
+			return nil, fmt.Errorf("could not write headers: %w", err)
+		}
+	}
+
+	return &CsvConnector{
+		File: filename,
+	}, nil
+}
+
+func NewCsvConnector(filename string, append bool) (*CsvConnector, error) {
+	return newCsvConnector(filename, append, nil)
+}
+
+func NewCsvConnectorWithHeaders(filename string, append bool, headers []string) (*CsvConnector, error) {
+	return newCsvConnector(filename, append, &headers)
+}
+
 func (c *CsvConnector) Send(data []string) error {
-	file, err := os.OpenFile(c.File, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(c.File, os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		return fmt.Errorf("could not open file: %w", err)
 	}
