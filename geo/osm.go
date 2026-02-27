481525c8c849515572bed7c204ab8f0bf69158fa
SwatBros <40635338+SwatBros@users.noreply.github.com>
2026-02-27 12:26:15 +0100

    wip(geo): handle multipolygon cities

--- geo/osm.go
+++ geo/osm.go
@@ -15,7 +15,17 @@ import (
 	"github.com/paulmach/osm/osmpbf"
 )
 
-func ListAndFilterCities(filepath string, names []string) {
+type TagFilter struct {
+	Tag   string
+	Value string
+}
+
+// Create a FeatureCollection with Multipolygons representing the area of filtered cities (admin level 8)
+//
+// filepath File path to an osm.pbf file
+//
+// names The tag and value list used to filter cities
+func ListAndFilterCities(filepath string, names []TagFilter) {
 	file, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
@@ -32,7 +42,9 @@ func ListAndFilterCities(filepath string, names []string) {
 		scanner.SkipWays = true
 
 		scanner.FilterRelation = func(r *osm.Relation) bool {
-			return r.Tags.Find("admin_level") == "8" && slices.Contains(names, r.Tags.Find("name"))
+			return r.Tags.Find("admin_level") == "8" && slices.ContainsFunc(names, func(filter TagFilter) bool {
+				return r.Tags.Find(filter.Tag) == filter.Value
+			})
 		}
 
 		for scanner.Scan() {
@@ -82,33 +94,50 @@ func ListAndFilterCities(filepath string, names []string) {
 		}
 	}
 
+	// Sort ways based on Relation member order
 	sort.Slice(wayResults, func(i, j int) bool {
 		a := wayResults[i]
 		b := wayResults[j]
 		return slices.Index(wayIds, a.WayId) < slices.Index(wayIds, b.WayId)
 	})
 
-	prev := &wayResults[0]
-	next := &wayResults[1]
-
-	lastNodeOfPrev := prev.NodeIds[len(prev.NodeIds)-1]
-	firstNodeOfNext := next.NodeIds[0]
-	lastNodeOfNext := next.NodeIds[len(next.NodeIds)-1]
-
-	if lastNodeOfPrev != firstNodeOfNext && lastNodeOfNext != lastNodeOfPrev {
-		slices.Reverse(prev.NodeIds)
+	ringGroups := []*RingResult{
+		{WayResult: []WayResult{wayResults[0]}},
 	}
 
-	for _, result := range wayResults[1:] {
-		if prev.NodeIds[len(prev.NodeIds)-1] != result.NodeIds[0] {
-			slices.Reverse(result.NodeIds)
+	// Group ways into rings
+	for _, wayResult := range wayResults[1:] {
+		found := false
+
+		for _, ringGroup := range ringGroups {
+			lastRingResult := ringGroup.WayResult[len(ringGroup.WayResult)-1]
+			firstRingNode := lastRingResult.NodeIds[0]
+			lastRingNode := lastRingResult.NodeIds[len(lastRingResult.NodeIds)-1]
+
+			firstResultNode := wayResult.NodeIds[0]
+			lastResultNode := wayResult.NodeIds[len(wayResult.NodeIds)-1]
+
+			if firstRingNode == firstResultNode ||
+				firstRingNode == lastResultNode ||
+				lastRingNode == firstResultNode ||
+				lastRingNode == lastResultNode {
+				ringGroup.WayResult = append(ringGroup.WayResult, wayResult)
+				found = true
+				break
+			}
 		}
-		prev = &result
+
+		if !found {
+			ringGroups = append(ringGroups, &RingResult{WayResult: []WayResult{wayResult}})
+		}
+	}
+
+	// Flip ways within each ring
+	for _, ringGroup := range ringGroups {
+		ringGroup.flipWays()
 	}
 
 	file.Seek(0, io.SeekStart)
-
-	var points = []PointResult{}
 
 	{
 		scanner := osmpbf.New(context.Background(), file, runtime.GOMAXPROCS(-1))
@@ -128,13 +157,15 @@ func ListAndFilterCities(filepath string, names []string) {
 		for scanner.Scan() {
 			switch o := scanner.Object().(type) {
 			case *osm.Node:
-				for _, result := range wayResults {
-					if slices.Contains(result.NodeIds, o.ID) {
-						points = append(points, PointResult{
-							Point:  orb.Point{o.Lon, o.Lat},
-							NodeId: o.ID,
-							WayId:  result.WayId,
-						})
+				for _, ringGroup := range ringGroups {
+					for _, result := range ringGroup.WayResult {
+						if slices.Contains(result.NodeIds, o.ID) {
+							ringGroup.Points = append(ringGroup.Points, PointResult{
+								Point:  orb.Point{o.Lon, o.Lat},
+								NodeId: o.ID,
+								WayId:  result.WayId,
+							})
+						}
 					}
 				}
 			case *osm.Way:
@@ -145,36 +176,13 @@ func ListAndFilterCities(filepath string, names []string) {
 		}
 	}
 
-	// Sort the ring following wayIds, nodeIds order
-	sort.Slice(points, func(i, j int) bool {
-		if points[i].WayId != points[j].WayId {
-			return slices.Index(wayIds, points[i].WayId) < slices.Index(wayIds, points[j].WayId)
-		}
-
-		var wayResult *WayResult
-		for _, result := range wayResults {
-			if result.WayId == points[i].WayId {
-				wayResult = &result
-				break
-			}
-		}
-
-		return slices.Index(wayResult.NodeIds, points[i].NodeId) < slices.Index(wayResult.NodeIds, points[j].NodeId)
-	})
-
 	collection := geojson.NewFeatureCollection()
-	lines := map[osm.WayID]orb.LineString{}
 
-	for _, point := range points {
-		lines[point.WayId] = append(lines[point.WayId], point.Point)
+	poly := orb.MultiPolygon{}
+	for _, ringGroup := range ringGroups {
+		poly = append(poly, orb.Polygon{ringGroup.OrmRing()})
 	}
 
-	ring := orb.Ring{}
-	for _, point := range points {
-		ring = append(ring, point.Point)
-	}
-
-	poly := orb.Polygon{ring}
 	feature := geojson.NewFeature(poly)
 
 	collection.Features = append(collection.Features, feature)
@@ -196,13 +204,95 @@ func ListAndFilterCities(filepath string, names []string) {
 	}
 }
 
+// A Point along a ring
+//
+// # Point Coordinates
+//
+// # NodeId The OSM NodeId
+//
+// WayId The OSM way the node belongs to
 type PointResult struct {
 	Point  orb.Point
 	NodeId osm.NodeID
 	WayId  osm.WayID
 }
 
+// An OSM way and its nodes
 type WayResult struct {
 	WayId   osm.WayID
 	NodeIds []osm.NodeID
+}
+
+// Informations about a Ring
+//
+// # WayResult List of ways that compose the ring
+//
+// Points List of points that compose the ring
+type RingResult struct {
+	WayResult []WayResult
+	Points    []PointResult
+}
+
+// Flip the ways so that they all point the same direction
+//
+// This is needed so that a polygon perimeter can be navigated
+// A - B -> B - C
+func (r *RingResult) flipWays() {
+	if len(r.WayResult) < 2 {
+		return
+	}
+
+	prev := &r.WayResult[0]
+	next := &r.WayResult[1]
+
+	lastNodeOfPrev := prev.NodeIds[len(prev.NodeIds)-1]
+	firstNodeOfNext := next.NodeIds[0]
+	lastNodeOfNext := next.NodeIds[len(next.NodeIds)-1]
+
+	if lastNodeOfPrev != firstNodeOfNext && lastNodeOfNext != lastNodeOfPrev {
+		slices.Reverse(prev.NodeIds)
+	}
+
+	for _, result := range r.WayResult[1:] {
+		if prev.NodeIds[len(prev.NodeIds)-1] != result.NodeIds[0] {
+			slices.Reverse(result.NodeIds)
+		}
+		prev = &result
+	}
+}
+
+// Sort the points along a way
+func (r *RingResult) sortPoints() {
+	wayIds := make([]osm.WayID, len(r.WayResult))
+	for i, result := range r.WayResult {
+		wayIds[i] = result.WayId
+	}
+
+	sort.Slice(r.Points, func(i, j int) bool {
+		if r.Points[i].WayId != r.Points[j].WayId {
+			return slices.Index(wayIds, r.Points[i].WayId) < slices.Index(wayIds, r.Points[j].WayId)
+		}
+
+		var wayResult *WayResult
+		for _, result := range r.WayResult {
+			if result.WayId == r.Points[i].WayId {
+				wayResult = &result
+				break
+			}
+		}
+
+		return slices.Index(wayResult.NodeIds, r.Points[i].NodeId) < slices.Index(wayResult.NodeIds, r.Points[j].NodeId)
+	})
+}
+
+// Build the ORM ring sorting the points
+func (r *RingResult) OrmRing() orb.Ring {
+	r.sortPoints()
+
+	points := make([]orb.Point, len(r.Points))
+	for i, point := range r.Points {
+		points[i] = point.Point
+	}
+
+	return orb.Ring(points)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/go-chi/chi/v5 v5.2.5
 	github.com/kelindar/noise v0.1.0
+	github.com/paulmach/orb v0.12.0
+	github.com/paulmach/osm v0.9.0
 )
 
 require (
@@ -13,8 +15,6 @@ require (
 	github.com/kelindar/bitmap v1.5.3 // indirect
 	github.com/kelindar/simd v1.1.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/paulmach/orb v0.12.0 // indirect
-	github.com/paulmach/osm v0.9.0 // indirect
 	github.com/paulmach/protoscan v0.2.1 // indirect
 	go.mongodb.org/mongo-driver v1.17.9 // indirect
 	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
