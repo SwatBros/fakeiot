6b259292928ce4bf882776048e8fc8f11322f459
Alessio Disarò <40635338+SwatBros@users.noreply.github.com>
2026-03-02 10:11:07 +0100

    feat: add time dependant random

--- random/random.go
+++ random/random.go
@@ -2,6 +2,12 @@ package random
 
 type Random interface {
 	Next() float64
+	Value() float64
+}
+
+type TimeDependantRandom interface {
+	Next(float64) float64
+	Value() float64
 }
 
 type MultiRandom struct {
@@ -16,6 +22,22 @@ func (m *MultiRandom) Next() float64 {
 	var sum float64
 	for _, p := range m.processes {
 		sum += p.Next()
+	}
+	return sum
+}
+
+type TimeDependantMultiRandom struct {
+	processes []TimeDependantRandom
+}
+
+func NewTimeDependantMultiRandom(processes ...TimeDependantRandom) *TimeDependantMultiRandom {
+	return &TimeDependantMultiRandom{processes: processes}
+}
+
+func (m *TimeDependantMultiRandom) Next(dt float64) float64 {
+	var sum float64
+	for _, p := range m.processes {
+		sum += p.Next(dt)
 	}
 	return sum
 }
